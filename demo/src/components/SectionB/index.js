31720e171ef40144f0bfb8a984a57d91c422dcf3
Nick Huynh <nick.huynh@uconn.edu>
2018-05-27 16:31:57 -0400

    Fixed NavBar and started adding in Member Cards

---
+++ demo/src/components/SectionB/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SectionB extends Component {
+  render() {
+    return (
+      <section id="section-b" className="grid">
+
+      </section>
+    )
+  }
+}
+
+export default SectionB
