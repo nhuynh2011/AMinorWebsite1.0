e35e31c662abd32d8a9acacee0b11541e9f70486
chs13010 <christopher.shank@uconn.edu>
2018-06-05 20:48:36 -0400

    NavLinks now toggle an .active class if that linked is pressed.

--- nextApp/components/header.js
+++ nextApp/components/header.js
@@ -1,7 +1,6 @@
 import { Component } from 'react'
 import NavLink from './navLink'
 
-
 export default class extends Component {
   constructor(props) {
     super(props)
@@ -9,7 +8,7 @@ export default class extends Component {
       activeLink: 'Home'
     }
     this.links = [
-      {title: 'Home', href: '/', isActive: true},
+      {title: 'Home', href: '/'},
       {title: 'News', href: '/news'},
       {title: 'History', href: '/history'},
       {title: 'Members', href: '/members'},
@@ -18,17 +17,21 @@ export default class extends Component {
     ]
   }
 
+  setActiveLink = (newActiveLink) => {
+    this.setState({ activeLink: newActiveLink })
+  }
+
   render() {
     return (
       <header>
         <nav>
           <ul>
             {this.links.map(link => <NavLink
-              isActive={link.isActive}
+              updateActiveLink={this.setActiveLink}
               href={link.href}
+              isActive={link.title === this.state.activeLink}
               key={link.title}
-              title={link.title}
-              />
+              title={link.title}/>
             )}
           </ul>
         </nav>
--- nextApp/components/navLink.js
+++ nextApp/components/navLink.js
@@ -2,31 +2,29 @@ import { Component } from 'react'
 import Link from 'next/link'
 
 export default class extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { isActive: undefined }
-  }
-
-  componentWillReceiveProps(newProps) {
-    this.setState({ isActive: newProps.isActive })
-  }
-
-  handleClick = (e) => {
-    console.log(e.target.innerHTML)
-    this.setState({isActive: true}, () => {console.log(this.state.isActive)})
+  handleClick = () => {
+    this.props.updateActiveLink(this.props.title)
   }
 
   render () {
     return (
       <li onClick={this.handleClick}>
         <Link href={this.props.href}>
-          <a onClick={this.handleClick}>{this.props.title}</a>
+          <a className={this.props.isActive ? "active" : ""}>{this.props.title}</a>
         </Link>
 
         <style jsx>
           {`
             li {
               margin: 15px;
+            }
+
+            a {
+              transition: color 1s ease-in-out;
+            }
+
+            .active {
+              color: green;
             }
           `}
         </style>
