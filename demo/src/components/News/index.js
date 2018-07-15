9726fd01643e94b837c02585371a38f2ae349f85
Nick Huynh <nick.huynh@uconn.edu>
2018-07-14 20:31:04 -0400

    Deleted demo, added footer

--- demo/src/components/News/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-class News extends Component {
-	render () {
-		return <div>News Page</div>
-	}
-}
-export default News
--- demo/src/components/Repertoire/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-class Repertoire extends Component {
-	render () {
-		return <div>Repertoire Page</div>
-	}
-}
-export default Repertoire
