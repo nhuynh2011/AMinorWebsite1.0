9031502afc1c0cb27dd2017c160449f117a6e555
chs13010 <christopher.shank@uconn.edu>
2018-08-26 11:23:28 -0400

    Updated repertoire page.

--- nextApp/pages/repertoire.js
+++ nextApp/pages/repertoire.js
@@ -1,14 +1,42 @@
 import fetch from 'isomorphic-fetch';
+import DescriptionBox from '../components/DescriptionBox';
 
 const REPERTOIRE_PAGE = ({ repertoire }) => (
-	<p>Rep</p>
+	<section>
+		<DescriptionBox
+			heading="Current Repertoire"
+			subtext="The following list of songs is the songs that we can sing at a gig or teach at a master class."
+		>
+		</DescriptionBox>
+
+		<ul>
+			{repertoire.past.map((song, index) => <li key={index}>{song}</li>)}
+		</ul>
+
+		<style jsx>
+			{
+				`
+					ul {
+						align-items: center;
+						display: flex;
+						flex-direction: column;
+						list-style-type: none;
+					}
+
+					li {
+						margin: 0.707rem;
+					}
+				`
+			}
+		</style>
+	</section>
 )
 
 REPERTOIRE_PAGE.getInitialProps = async () => {
-	const response  = await fetch('https://0zw0wj7m2i.execute-api.us-east-1.amazonaws.com/TestGet/content?page=repertoire')
-	const data = await response.json()
-	const repertoire = data.message.content
-	return { repertoire }
+	const response  = await fetch('https://0zw0wj7m2i.execute-api.us-east-1.amazonaws.com/TestGet/content?page=repertoire');
+	const data = await response.json();
+	const repertoire = data.message.content;
+	return { repertoire };
 }
 
 export default REPERTOIRE_PAGE;
